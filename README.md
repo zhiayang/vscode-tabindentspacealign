ac267cb754a406cf034aa7cc15190792bdd333ba
zhiayang <zhiayang@gmail.com>
2018-01-07 01:53:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Dynamic Tab Insertion
+
+This extension augments the functionality of the 'tab' key. If you're the kind of person to use tabs for indentations and spaces for alignment, then
+this is the plugin for you!
+
+## Features
+
+If your 'tab' key inserts spaces, then this plugin will not do anything. Otherwise, when pressing the 'tab' key:
+
+1. Before a non-whitespace character appears on a line (ie. indentation), the tab literal is inserted.
+2. After any non-whitespace character appears (ie. inline tabs), spaces are inserted.
+
+The spaces inserted will align you to the nearest tab stop, based on your preferred tab size.
+
+There are no options to configure, but this plugin adds a keybinding to the 'tab' key in the editor.
+
+
+## Known Issues
+
+1. Not really an issue, but there might be a slight performance impact since VS Code offers no builtin method to get the *column* that the cursor is at,
+rather only the character index.
+
+## Release Notes
+
+### 1.0.1
+
+Name change
+
+### 1.0.0
+
+Initial release
---
+++ src/extension.ts
@@ -0,0 +1,78 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { window, commands, Disposable, TextDocument, ExtensionContext, TextEditorEdit } from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: ExtensionContext)
+{
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('tabindentspacealign plugin active');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+
+	if(window.activeTextEditor && window.activeTextEditor.options.insertSpaces)
+		return;
+
+	let disposable = commands.registerCommand('extension.dynamictabkey', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Get the current text editor
+		let editor = window.activeTextEditor;
+		if(!editor) { return; }
+
+		let doc = editor.document;
+		let cursorpos = editor.selection.active;
+
+		let curline = doc.lineAt(cursorpos);
+		let k = curline.firstNonWhitespaceCharacterIndex;
+
+		// if the first non-whitespace character is after the cursor position, then we insert tabs
+		if(k >= cursorpos.character)
+		{
+			// insert a tab
+			editor.edit((eb: TextEditorEdit) => {
+				editor.selections.forEach(it => {
+					eb.delete(it);
+					eb.insert(it.start, '\t');
+				});
+			});
+		}
+		else
+		{
+			// insert spaces
+			let tabsz = (editor.options.tabSize as number);
+
+			editor.edit((eb: TextEditorEdit) => {
+				editor.selections.forEach(it => {
+					eb.delete(it);
+
+					let line = editor.document.lineAt(it.start.line).text;
+
+					let cursor = 0;
+					for (let idx = 0; idx <= it.start.character; idx++)
+					{
+						if(line[idx] == '\t') cursor += tabsz;
+						else                  cursor++;
+					}
+
+					let finalpos = Math.ceil(cursor / tabsz) * tabsz;
+
+					// console.log("tab size: %d, cursor: %d / %d, finalpos: %d", tabsz, cursor, it.active.character, finalpos);
+					eb.insert(it.start, ' '.repeat(finalpos - cursor + 1));
+				});
+			});
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate()
+{
+}
